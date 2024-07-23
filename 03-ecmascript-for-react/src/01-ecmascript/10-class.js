25440bdd4510c16e96e5a9715058a605849d9973
ttining <ttining.real@gmail.com>
2024-07-23 15:01:02 +0900

    [학습] ES 클래스 구문

--- 03-ecmascript-for-react/src/01-ecmascript/10-class.js
+++ 03-ecmascript-for-react/src/01-ecmascript/10-class.js
@@ -2,10 +2,13 @@
 // ES5 Constructor (like. Class) with Module Pattern (IIFE)
 
 const _ApiClient = (function () {
+  // Class
+  // constructor function
   function ApiClient(endpoint) {
     this._endpoint = endpoint;
   }
 
+  // Instance Methods
   ApiClient.prototype.readAll = function (page = 1, pageSize = 5) {
     return fetch(`${this._endpoint}?_page=${page}&_limit=${pageSize}`).then(
       (response) => response.json()
@@ -41,6 +44,49 @@ const _ApiClient = (function () {
 
 // 🔶 Class 구문을 사용해 ApiClient 클래스를 작성합니다.
 // 참고: https://mzl.la/3UdwrNE
+
+class ApiClient {
+  // 비공개 멤버
+  #endpoint;
+
+  // 생성자 함수
+  // 생성 시점에서 1회 실행
+  constructor(endpoint) {
+    this.#endpoint = endpoint;
+  }
+
+  // 인스턴스(객체) 메서드
+  // 클래스로부터 생성된 객체가 공유하는 메서드(생성된 객체가 소유하는 함수)
+  readAll(page = 1, pageSize = 5) {
+    return fetch(`${this.#endpoint}?_page=${page}&_limit=${pageSize}`).then(
+      (response) => response.json()
+    );
+  }
+
+  readOne(id) {
+    return fetch(`${this.#endpoint}/${id}`).then((response) => response.json());
+  }
+
+  create(data) {
+    return fetch(this.#endpoint, {
+      method: 'POST',
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
+  }
+
+  update(id, data) {
+    return fetch(`${this.#endpoint}/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
+  }
+
+  delete(id) {
+    return fetch(`${this.#endpoint}/${id}`, {
+      method: 'DELETE',
+    }).then((response) => response.json());
+  }
+}
 
 const todosService = new _ApiClient(
   'https://jsonplaceholder.typicode.com/todos'
