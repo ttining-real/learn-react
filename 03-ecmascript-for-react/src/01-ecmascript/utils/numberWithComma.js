21b1bcefc805b7082ea75bd3492920e985a8a057
ttining <ttining.real@gmail.com>
2024-07-24 17:45:56 +0900

    [학습] ES 모듈 - 모듈 내보내기 - 모듈 가져오기 - 모듈 다시 내보내기 - 모듈 별칭 이름 설정

--- 03-ecmascript-for-react/src/01-ecmascript/utils/numberWithComma.js
+++ 03-ecmascript-for-react/src/01-ecmascript/utils/numberWithComma.js
@@ -1,7 +1,7 @@
 // 이름 내보내기 (named export)
 /**@type {(n: number) => string} */
 function numberWithComma(n) {
-  return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 }
 
 // 기본 내보내기 (default export)
