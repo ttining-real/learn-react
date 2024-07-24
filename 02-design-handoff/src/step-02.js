255e9208bafc586e3e2694adb8d9074f969059fe
ttining <ttining.real@gmail.com>
2024-07-24 17:48:47 +0900

    [학습] 클래스 프로그래밍 - ECMAScript Class 구문 - 객체 지향 프로그래밍(OOP) - OOJS (Prototype based)

---
+++ 02-design-handoff/src/step-02.js
@@ -0,0 +1,13 @@
+// ---------------------------------------------------------
+// [ ] STEP 1. Vanilla Script (ES + DOM API)
+// [x] STEP 2. Class Programming
+// [ ] STEP 3. Web Components API
+// ---------------------------------------------------------
+import SortableList from './SortableList.js';
+
+// Class => Instance
+// API 1. new Class()
+// API 2. Class.create()
+const sortableList = new SortableList('.list');
+
+console.log(SortableList);
---
+++ 02-design-handoff/src/step-03.js
@@ -0,0 +1,15 @@
+// ---------------------------------------------------------
+// [ ] STEP 1. Vanilla Script (ES + DOM API)
+// [ ] STEP 2. Class Programming
+// [x] STEP 3. Web Components API
+// ---------------------------------------------------------
+
+// Custom Element
+
+class SortableList extends HTMLElement {
+    // constructor() {
+    //     super()
+    // }
+}
+
+customElements.define('sortable-list', SortableList);
