0442100975141fe290754066d387d5692f104271
ttining <ttining.real@gmail.com>
2024-07-26 16:50:58 +0900

    [학습] JSX 트랜스폼 (with Babel)

--- 06-react-with-jsx-transform/src/main.js
+++ 06-react-with-jsx-transform/src/main.js
@@ -1,24 +1,68 @@
 import React from 'https://esm.sh/react';
 import { createRoot } from 'https://esm.sh/react-dom';
-// import { ArchitectureListPage } from './pages/ArchitectureListPage.js';
-import AvatarListPage from './pages/AvatarListPage.js';
 
-// 리액트 앱을 렌더링 할 DOM 요소 참조
+function Avatar({ name, photo, status = 'offline', size = 64 }) {
+  let iconPath = '';
+  let statusMessage = '';
+
+  switch (status) {
+    default:
+    case 'offline':
+      iconPath = '/icons/status-offline.svg';
+      statusMessage = '오프라인';
+      break;
+    case 'online':
+      iconPath = '/icons/status-online.svg';
+      statusMessage = '온라인';
+      break;
+    case 'dont-disturb':
+      iconPath = '/icons/status-dont-disturb.svg';
+      statusMessage = '방해금지';
+      break;
+    case 'away':
+      iconPath = '/icons/status-away.svg';
+      statusMessage = '자리비움';
+      break;
+  }
+
+  const label = `${name} (${statusMessage})`;
+
+  // JSX
+  return (
+    <figure className="Avatar" aria-label={label} title={label}>
+      <img src={`/faces/${photo}`} alt={name} width={size} height={size} />
+      <figcaption>
+        <img src={iconPath} alt="" />
+      </figcaption>
+    </figure>
+  )
+}
+
+function AvatarListPage() {
+
+  // JSX
+  return (
+    <ul className="AvatarList">
+      <li>
+        <Avatar name="야무" photo="man-02.jpg" status="online" />
+      </li>
+      <li>
+        <Avatar name="범쌤" photo="man-04.jpg" status="away" />
+      </li>
+      <li>
+        <Avatar name="주원" photo="woman-04.jpg" status="dont-disturb" />
+      </li>
+      <li>
+        <Avatar name="정민" photo="woman-01.jpg" />
+      </li>
+    </ul>
+  )
+}
+
 const container = document.getElementById('react-app');
 
-// DOM 요소가 존재한다면?
 if (container) {
-  // DOM 요소를 리액트 돔 루트로 만든 후, 리액트 앱 렌더링
-  // 페이지 컴포넌트 렌더링
-  createRoot(container).render(React.createElement(AvatarListPage));
-}
-// 존재하지 않는다면?
-else {
-  // 개발자에게 경고
+  createRoot(container).render(<AvatarListPage />);
+} else {
   console.warn('문서에 "#app" 요소가 존재하지 않습니다.');
 }
-
-// TODO: Avatar 컴포넌트 및 페이지 컴포넌트 작성 후 화면에 표시(렌더링)
-// 1. 페이지 컴포넌트 작성 및 main.js 연결
-// 2. Avatar 컴포넌트 작성 및 status 속성(prop) 설정
-// 2. status 속성(prop)에 따라 화면에 표시되는 Avatar 구성
