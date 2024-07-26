4beef02aff20db3cef2cf9d021e244da09c985fc
ttining <ttining.real@gmail.com>
2024-07-26 11:33:25 +0900

    [학습] ArchitectureList 함수 컴포넌트 작성

--- 05-react-component-with-props/src/main.js
+++ 05-react-component-with-props/src/main.js
@@ -7,6 +7,7 @@ import listData from './data/list.js';
 // 컴포넌트 불러오기
 import NumberList from './components/NumberList.function.js';
 import ArchitectureList from './components/architectures/ArchitectureList.class.js';
+import ArchitectureListFunc from './components/architectures/ArchitectureList.function.js';
 import ArchitectureItem from './components/architectures/ArchitectureItem.class.js';
 
 // 리액트 앱을 렌더링 할 DOM 요소 참조
@@ -27,8 +28,14 @@ if (container) {
   // NumberList 컴포넌트에 속성(props) 전달
   const numberList = React.createElement(NumberList, { count: 9 });
 
+  // ArchitectureListFunc 컴포넌트 -> 리액트 엘리먼트 생성
+  // 필요한 경우 컴포넌트 속성(props)를 전달
+  const architectureListFunc = React.createElement(ArchitectureListFunc, {
+    lang: 'en',
+  });
+
   // DOM 요소를 리액트 돔 루트로 만든 후, 리액트 앱 렌더링
-  createRoot(container).render(architectureList);
+  createRoot(container).render(architectureListFunc);
 }
 // 존재하지 않는다면?
 else {
