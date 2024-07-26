c23deeb4eddc88ed3d1b0ec88ddab652489dfc88
ttining <ttining.real@gmail.com>
2024-07-26 11:12:23 +0900

    [학습] NumberList 함수 컴포넌트 작성

--- 05-react-component-with-props/src/main.js
+++ 05-react-component-with-props/src/main.js
@@ -5,7 +5,7 @@ import { createRoot } from 'https://esm.sh/react-dom';
 import listData from './data/list.js';
 
 // 컴포넌트 불러오기
-import NumberList from './components/NumberList.class.js';
+import NumberList from './components/NumberList.function.js';
 import ArchitectureList from './components/architectures/ArchitectureList.class.js';
 import ArchitectureItem from './components/architectures/ArchitectureItem.class.js';
 
@@ -14,7 +14,6 @@ const container = document.getElementById('react-app');
 
 // DOM 요소가 존재한다면?
 if (container) {
-
   // ArchitectureList 컴포넌트 -> 리액트 엘리먼트 생성
   // ArchitectureList 컴포넌트에 속성(props) 전달
   const architectureList = React.createElement(ArchitectureList, {
@@ -24,9 +23,13 @@ if (container) {
     ),
   });
 
+  // NumberList 컴포넌트 -> 리액트 엘리먼트 생성
+  // NumberList 컴포넌트에 속성(props) 전달
+  const numberList = React.createElement(NumberList, { count: 9 });
+
   // DOM 요소를 리액트 돔 루트로 만든 후, 리액트 앱 렌더링
   createRoot(container).render(architectureList);
-} 
+}
 // 존재하지 않는다면?
 else {
   // 개발자에게 경고
