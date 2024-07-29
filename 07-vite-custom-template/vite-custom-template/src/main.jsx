dca55346dd724c003e4c1dc2e0529fd1def14aa3
ttining <ttining.real@gmail.com>
2024-07-29 16:43:38 +0900

    [튜토리얼] Vite 커스텀 템플릿 Part 7.
    - 절대 경로 구성 (@)
    - 개발 서버 구성

--- 07-vite-custom-template/vite-custom-template/src/main.jsx
+++ 07-vite-custom-template/vite-custom-template/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import '@/styles/globals.css';
+import App from '@/App';
 
 // Vite 클라이언트 환경(브라우저)에서 사용 가능한 환경 변수
 // console.log(import.meta.env);
