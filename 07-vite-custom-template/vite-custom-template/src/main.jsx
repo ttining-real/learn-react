e35c28d8b34fb81a11b55a27e1a35b6ad3375574
ttining <ttining.real@gmail.com>
2024-07-29 11:40:52 +0900

    [튜토리얼] Vite 커스텀 템플릿 Part 2.
    - react, react-dom 패키지 설치
    - src/main.jsx 리액트 앱 렌더링

---
+++ 07-vite-custom-template/vite-custom-template/src/main.jsx
@@ -0,0 +1,19 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+console.log(React.version, ReactDOM);
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>React 웹 앱</h1>
+    </div>
+  );
+}
+
+const domNode = document.getElementById('react-app');
+createRoot(domNode).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
