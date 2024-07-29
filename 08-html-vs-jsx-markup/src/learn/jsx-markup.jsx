54d18d20c16a0ad57da2c342cdc931c278c1cd99
ttining <ttining.real@gmail.com>
2024-07-29 16:52:49 +0900

    [학습] JSX 마크업 Part 1.
    - 멀티 라인
    - 래퍼 엘리먼트 (React.Fragment)

--- 08-html-vs-jsx-markup/src/learn/jsx-markup.jsx
+++ 08-html-vs-jsx-markup/src/learn/jsx-markup.jsx
@@ -1,13 +1,9 @@
+import DataBinding from './data-binding';
+
 function JSX_Markup() {
   return (
     <dl className="descriptionList">
-      <dt>데이터 바인딩(data binding)</dt>
-      <dd>
-        <p>상태 메시지(status message)를 연결해 화면에 출력합니다.</p>
-        <span className="status">
-          {/* statusMessage 값을 화면에 표시합니다. (랜덤 표시도 도전!) */}
-        </span>
-      </dd>
+      <DataBinding />
       <dt>조건부 렌더링(conditional rendering)</dt>
       <dd>
         <p>이미지 타입(image type)에 따라 렌더링 여부를 결정합니다.</p>
