c33ce839721796f2d003d65637210ad2c0781981
ttining <ttining.real@gmail.com>
2024-07-31 21:41:35 +0900

    [학습] JSX 마크업 Part 3.2.
    - 유틸리티 함수 typeOf
    - 컴포넌트 속성 검사 (컴포넌트 타입에 문제가 있으면 콘솔에 오류 표시)

--- 08-html-vs-jsx-markup/src/learn/data-binding.jsx
+++ 08-html-vs-jsx-markup/src/learn/data-binding.jsx
@@ -1,10 +1,17 @@
-import { randomNumber } from '../utils';
+import { randomNumber, typeOf } from '../utils';
 
 function DataBinding({ statusMessages }) {
   // [미션] 랜덤 로직을 작성해서 임의의 상태 메시지가 표시되도록 설정합니다.
-
+  console.log(statusMessages);
   const statusMessage =
     statusMessages[randomNumber(0, statusMessages.length - 1)];
+
+  // 리액트에서 이렇게 하는 거 아닙니다!!!
+  // 전달된 props의 각 속성 타입 검사
+  // if (!Array.isArray(statusMessages)) {
+  //   console.warn('statusMessages가 배열이 아니야! 다시 확인해~');
+  //   return null;
+  // }
 
   return (
     <>
@@ -21,3 +28,32 @@ function DataBinding({ statusMessages }) {
 }
 
 export default DataBinding;
+
+// 컴포넌트 속성 타입 검사
+// Prop Types Validation
+
+// 리액트가 제공하는 방법
+// Component.propTypes
+
+DataBinding.propTypes = {
+  statusMessages(props, propName, componentName) {
+    // 컴포넌트 속성의 값은?
+    const propValue = props[propName];
+
+    // 컴포넌트 속성 값의 타입은? (문자 값을 원함)
+    const propType = typeOf(propValue); // 'array'
+
+    // 허용할 데이터 타입 이름은?
+    const allowedType = 'array';
+
+    // 검사 수행
+    if (propType !== allowedType) {
+      // 오류가 있으면 오류 메시지 출력
+      // 메시지는 `[      ] 컴포넌트 [     ] 속성 타입은 "[    ]" 타입이 요구되나, 실제 전달된 타입은 "[    ]"입니다.`
+      throw new Error(
+        `${componentName} 컴포넌트 ${propName} 속성 타입은 "${allowedType}" 타입이 요구되나, 실제 전달된 타입은 "${propType}"입니다.`
+      );
+    }
+    // 오류가 없으면 패스
+  },
+};
