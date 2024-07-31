ac7631f8a980b35db926150e474a07f186eea415
ttining <ttining.real@gmail.com>
2024-07-31 16:36:47 +0900

    [과제] Sass 설치
    - Sass base + utils 추가

--- 00-homework/03-custom-scaffold/src/main.jsx
+++ 00-homework/03-custom-scaffold/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/test/App.jsx';
+import '@/style/main.scss';
 
 const domNode = document.getElementById('react-app');
 createRoot(domNode).render(
