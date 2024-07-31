375a2dc29311a614d293da009e15feeb561e7f67
ttining <ttining.real@gmail.com>
2024-08-01 08:50:27 +0900

    [구성] 학습 자료
    - 09-component-purity
    - 10-responding-to-events
    - 11-declarative-programming

--- 09-responding-to-events/src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export * from './typeIs';
-export * from './randomNumber';
-export { default as PropTypes } from './propTypes';
--- 09-responding-to-events/src/utils/typeIs.js
+++
@@ -1,3 +0,0 @@
-export function typeIs(data) {
-  return Object.prototype.toString.call(data).slice(8, -1).toLowerCase();
-}
