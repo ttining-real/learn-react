7a7990aca3f50162e06e2f68e7ce230b062ef3c1
ttining <ttining.real@gmail.com>
2024-07-31 20:35:11 +0900

    [학습] JSX 마크업 Part 3.
    - 데이터 바인딩 (보간: 사이를 채우다)

--- 08-html-vs-jsx-markup/src/learn/data-binding.jsx
+++ 08-html-vs-jsx-markup/src/learn/data-binding.jsx
@@ -1,5 +1,17 @@
-function DataBinding(props) {
-  console.log(props);
+function DataBinding({ statusMessages }) {
+  // [미션] 랜덤 로직을 작성해서 임의의 상태 메시지가 표시되도록 설정합니다.
+  // JavaScript 프로그래밍
+  // Math.random() / Math.floor() / Math.round()
+
+  // console.log(statusMessages);
+
+  function randomNumber(min = 0, max = 10) {
+    if (min > max) throw new Error('max 보다 min 값이 큽니다.');
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
+  const statusMessage =
+    statusMessages[randomNumber(0, statusMessages.length - 1)];
 
   return (
     <>
@@ -8,13 +20,11 @@ function DataBinding(props) {
         <p>상태 메시지(status message)를 연결해 화면에 출력합니다.</p>
         <span className="status">
           {/* statusMessage 값을 화면에 표시합니다. (랜덤 표시도 도전!) */}
+          {statusMessage}
         </span>
       </dd>
     </>
   );
 }
-
-// JSX runtime (classic): React.createElement(React.Fragment, props, children)
-// JSX runtime (automatic): jsx(React.Fragment, props)
 
 export default DataBinding;
