270a6852f9b19527435c97349e49b527e3f61dc9
ttining <ttining.real@gmail.com>
2024-08-02 15:01:23 +0900

    [학습] 이벤트 전파 I
    - 이벤트 전파
    - 이벤트 캡쳐링
    - 이벤트 버블링

--- 10-responding-to-events/src/learn/responding-to-events/event-propagation.jsx
+++ 10-responding-to-events/src/learn/responding-to-events/event-propagation.jsx
@@ -6,15 +6,45 @@
 // - [ ] 기본 작동 방지
 // --------------------------------------------------------------------------
 
+// RGB 모드
+// CMYK 모드
+
+// event.preventDefault(); // 이벤트 기본 작동을 차단
+
+// Event Delegation (위임)
+// Event Propagation (전파)
+// event.stopPropagation(); // 이벤트 전파 중지
+
 function EventPropagation() {
   return (
     <details>
       <summary>
         <b>이벤트 전파 &amp; 기본 작동 방지</b>
       </summary>
-      <div className="box" style={styles.cyan}>
-        <div className="box" style={styles.magenta}>
-          <div className="box" style={styles.yellow}></div>
+      {/* 상위 컴포넌트 : 정민 */}
+      <div
+        onClick={(e) => {
+          console.log('cyan', e.target);
+        }}
+        className="box"
+        style={styles.cyan}
+      >
+        {/* 하위 컴포넌트 : 동호 */}
+        <div
+          onClick={(e) => {
+            console.log('magenta', e.target);
+          }}
+          className="box"
+          style={styles.magenta}
+        >
+          {/* 자손 컴포넌트 : 재명 */}
+          <div
+            onClick={(e) => {
+              console.log('yellow', e.target);
+            }}
+            className="box"
+            style={styles.yellow}
+          ></div>
         </div>
       </div>
     </details>
