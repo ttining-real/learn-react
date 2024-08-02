a42ad4efd08a013a2acf34ef51d8ff109cee9f63
ttining <ttining.real@gmail.com>
2024-08-02 14:21:45 +0900

    [학습] 이벤트 핸들러 추가
    - 이벤트 핸들러에서 사이드 이펙트 처리 가능
    - 이벤트 핸들러에서 컴포넌트 속성(props) 읽기
    - 이벤트 핸들러(함수)를 하위 컴포넌트에 속성(prop)으로 전달

--- 10-responding-to-events/src/learn/View.jsx
+++ 10-responding-to-events/src/learn/View.jsx
@@ -4,13 +4,22 @@ import EventHandlerProp from './responding-to-events/event-handler-prop';
 import EventPropagation from './responding-to-events/event-propagation';
 import EventWithSideEffects from './responding-to-events/event-with-side-effects';
 
+// 상위 컴포넌트
+// ⬇ props로 데이터 전달
+// 하위 컴포넌트
 function RespondingToEvents() {
+  // 함수 지역 변수 (데이터)
+  const message = '김사부! "집중~~"';
+
+  // 함수 내부의 지역 함수 (데이터)
+  const printMessage = () => console.log(message);
+
   return (
     <div className="ViewRespondingToEvent">
       <h1>이벤트에 응답</h1>
       <p>사용자와 상호작용하도록 이벤트를 구성합니다.</p>
       <hr />
-      <EventHandlerProp />
+      <EventHandlerProp onPrintMessage={printMessage} />
       <hr />
       <EventPropagation />
       <hr />
@@ -31,7 +40,7 @@ function HTMLvsJSX() {
 
 // --------------------------------------------------------------------------
 
-function View() {} 
+function View() {}
 
 View.HTMLvsJSX = HTMLvsJSX;
 View.RespondingToEvents = RespondingToEvents;
