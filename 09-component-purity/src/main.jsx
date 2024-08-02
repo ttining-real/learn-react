16c7fb064c38b8477201596096837211e03a6e2b
ttining <ttining.real@gmail.com>
2024-08-02 10:50:33 +0900

    [학습] 리액트 컴포넌트의 순수성과 <StrictMode>

--- 09-component-purity/src/main.jsx
+++ 09-component-purity/src/main.jsx
@@ -2,7 +2,7 @@
 // import './learn/about-side-effects.js';
 
 import '@/styles/main.css';
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
@@ -11,7 +11,7 @@ const container = document.getElementById('react-app');
 if (!container) throw new Error('문서에 "#app" 요소가 존재하지 않습니다.');
 
 createRoot(container).render(
-  <>
+  <StrictMode>
     <App />
-  </>
+  </StrictMode>
 );
