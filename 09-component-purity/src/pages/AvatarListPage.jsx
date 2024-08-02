cc9af7542c8c64ef29918ccf8a1cdc5c64ffff37
ttining <ttining.real@gmail.com>
2024-08-02 11:22:24 +0900

    [학습] 리액트 렌더링 프로세스와 관련 없는 코드

--- 09-component-purity/src/pages/AvatarListPage.jsx
+++ 09-component-purity/src/pages/AvatarListPage.jsx
@@ -14,12 +14,42 @@ AvatarListPage.propTypes = {
   list: arrayOf(UserType).isRequired,
 };
 
-function AvatarListPage({ list }) {
+const anotherData = {
+  count: 10,
+};
+
+function AvatarListPage(props) {
+  // 컴포넌트 내부 공간에서의 코드 로직은
+  // 동일 입력인 경우 동일 출력이 되어야 한다.
+
+  // 배열 객체의 pop() 메서드 사용
+  // 입력받은 데이터(props, 원본 데이터) - 뮤테이션(mutation, 수정)
+  // 직접 수정
+  // list.pop();
+  // props.list.pop();
+
+  // 외부의 데이터인 props에 의존한 파생된 데이터
+  // props는 읽기 전용 데이터이므로 전달 받은 하위 컴포넌트에서 수정하면 안된다.
+  const myList = [...props.list];
+
+  // 로컬 뮤테이션 (지역 내, 수정) 허용
+  // myList.pop();
+
+  // 리액트 렌더링 프로세스와 관련 없는 코드
+  // 부수 효과(side effects)
+  // DOM 요소에 대한 접근/조작
+  // 타이머 API 활용
+  let renderCount = 0;
+  setInterval(() => {
+    console.log(renderCount);
+    document.getElementById('react-app').dataset.render = ++renderCount;
+  }, 1000);
+
   return (
     <ul className="AvatarList">
-      {list.map((user) => (
+      {myList.map((user) => (
         <li key={user.id}>
-          <Avatar user={user} />
+          <Avatar user={user} /> {anotherData.count++}
         </li>
       ))}
     </ul>
