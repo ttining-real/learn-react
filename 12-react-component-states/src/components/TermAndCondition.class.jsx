a48446e9e6fcff3bac0123abe0829f670119cd60
ttining <ttining.real@gmail.com>
2024-08-05 13:24:54 +0900

    [학습] 이용 약관 선언적 제어 Step 3
    - 레거시 리액트 프로그래밍 (with Class 컴포넌트)

--- 12-react-component-states/src/components/TermAndCondition.class.jsx
+++ 12-react-component-states/src/components/TermAndCondition.class.jsx
@@ -7,8 +7,26 @@
 import { Component } from 'react';
 import './TermAndConditions.css';
 
+// 레거시 리액트 프로그래밍 (with Class 컴포넌트)
 class TermAndConditions extends Component {
+  constructor(props) {
+    // 컴포넌트에 전달된 속성(props)는 읽기 전용
+    super(props);
+
+    // 컴포넌트의 데이터 기억(메모리)
+    // 컴포넌트 속성(props)와 달리 상태 값 수정 가능
+    this.state = {
+      agreement: false,
+    };
+  }
+
   render() {
+    // data (props or state) => JSX (generating markup)
+    const { agreement } = this.state; // { agreement: boolean }
+
+    // 컴포넌트 속성 또는 상태로부터 파생된 상태 (derived state: from state or props)
+    const isDisabled = agreement ? false : true;
+
     return (
       <form className="TermAndConditions" onSubmit={this.handleSubmit}>
         <h2>이용 약관</h2>
@@ -22,11 +40,14 @@ class TermAndConditions extends Component {
             id="terms"
             name="terms"
             type="checkbox"
+            checked={agreement}
             onChange={this.handleCheck}
-          />
+          />{' '}
           <label htmlFor="terms">이용 약관에 동의합니다.</label>
         </div>
-        <button type="submit">확인</button>
+        <button type="submit" disabled={isDisabled}>
+          확인
+        </button>
       </form>
     );
   }
@@ -35,15 +56,20 @@ class TermAndConditions extends Component {
     e.preventDefault();
   };
 
+  // 사용자에 의한 액션(행동)에 의해
+  // 선언된 리액트 컴포넌트의 상태 업데이트
   handleCheck = (e) => {
-    // 이벤트 핸들러 내부에는 [ 부수 효과 ]을 작성할 수 있다.
-    // 명령형 프로그래밍 (리액트 렌더링 프로세스와 관련 없음)
-    const submitButton = document.querySelector('[type="submit"]');
-    if (e.target.checked) {
-      submitButton.removeAttribute('disabled');
-    } else {
-      submitButton.setAttribute('disabled', 'true');
-    }
+    const { checked: nextAgreement } = e.target;
+    // 리액트에게 렌더 트리거(요청)
+    // 클래스 컴포넌트에서의 상태 업데이트 API
+    // this.setState(nextState)
+
+    // console.log('previous state: ', this.state);
+
+    this.setState({ agreement: nextAgreement }, () => {
+      // console.log('updated state and callback');
+      console.log('updated state: ', this.state);
+    });
   };
 }
 
