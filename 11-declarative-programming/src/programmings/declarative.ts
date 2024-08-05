a401b3f97fdf3a5295c1dd3fe7aa1156c29a1c40
ttining <ttining.real@gmail.com>
2024-08-05 10:40:50 +0900

    [학습] 선언적 프로그래밍 (WHAT) ll
    - 선언된 상태 업데이트
    - 화면 업데이트 (도구에 의해)

--- 11-declarative-programming/src/programmings/declarative.ts
+++ 11-declarative-programming/src/programmings/declarative.ts
@@ -1,34 +1,52 @@
 // --------------------------------------------------------------------------
-// ✅ 선언형 프로그래밍
+// ✅ 선언적 프로그래밍
 // --------------------------------------------------------------------------
-// - [ ] 선언적 프로그래밍을 가능하게 하는 도구 사용 필요 (createState 함수 활용)
-// - [ ] 체크박스 활성 상태를 선언합니다. (상태 선언)
-// - [ ] 체크박스 및 버튼을 렌더링 하는 함수를 작성합니다. (렌더링 함수 작성)
-// - [ ] 선언된 상태가 변경되면, 체크박스와 버튼을 다시 렌더링합니다. (다시 렌더링)
+// - [x] 선언적 프로그래밍을 가능하게 하는 도구 사용 필요 (createState 함수 활용)
+// - [x] 체크박스 활성 "상태를 선언"합니다. (상태 선언)
+// - [x] 체크박스 및 버튼을 "렌더링 하는 함수를 작성"합니다. (렌더링 함수 작성)
+// - [ ] "선언된 상태가 변경"되면, 체크박스와 버튼을 "다시 렌더링"합니다. (다시 렌더링)
 // --------------------------------------------------------------------------
 
 import createState from "../lib/createState";
 
-// 상태로 사용할 데이터 정의
+// 무엇(WHAT)을 상태로 선언할 것인가?
 const data = {
   checked: false,
 };
 
-// 상태 업데이트가 감지되면 실행될 콜백 함수
-const render = () => {};
+// 리액트
+// 선언적 상태 변경 감지 -> 설정된 렌더 함수 실행
+// UI 업데이트 제어
+// 컴포넌트 렌더링 프로세스 (작동, 다시 작동 ....)
+const render = () => {
+  const { checked } = state;
+  // console.log('[checked 업데이트]:', checked);
+  // UI 업데이트(다시 렌더링)
+  (checkbox as HTMLInputElement).checked = checked;
+  if (checked) {
+    button.removeAttribute("disabled");
+    button.textContent = "활성 상태";
+  } else {
+    button.setAttribute("disabled", "true");
+    button.textContent = "비활성 상태";
+  }
+};
 
-// const stateValue = createState(data, render); // [state, setState]
-// 상태 선언
+const update = (globalThis.update = (value: boolean): void => {
+  setState("checked", value);
+});
+
 const [state, setState] = createState(data, render);
 
-// 선언된 상태 확인
-console.log(state.checked);
+// 최초 렌더링
+// render();
 
-// 상태 업데이트 (다음 번 상태 업데이트 값 설정)
-const nextCheckedValue = !state.checked;
-// console.log({ nextCheckedValue });
+const container = document.getElementById("declarative-programming");
+const checkbox = container.querySelector('input[type="checkbox"]');
+const button = container.querySelector("button");
 
-setState("checked", nextCheckedValue);
-
-// 선언된 상태 확인
-console.log(state.checked);
+checkbox.addEventListener("change", (e: Event) => {
+  const { checked: nextCheckedValue } = e.target as HTMLInputElement;
+  // 상태 업데이트 시도
+  update(nextCheckedValue);
+});
