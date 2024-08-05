2edbc947571d35173e8f3801ffe6e1f1959f8055
ttining <ttining.real@gmail.com>
2024-08-05 11:51:41 +0900

    [학습] 이용 약관 선언적 제어 Step 2
    - 명령형 프로그래밍 검토
    - 이벤트 핸들러 내부에 작성

--- 12-react-component-states/src/components/TermAndCondition.class.jsx
+++ 12-react-component-states/src/components/TermAndCondition.class.jsx
@@ -10,7 +10,7 @@ import './TermAndConditions.css';
 class TermAndConditions extends Component {
   render() {
     return (
-      <form className="TermAndConditions">
+      <form className="TermAndConditions" onSubmit={this.handleSubmit}>
         <h2>이용 약관</h2>
         <p>
           OOO 서비스를 이용함으로써 귀하는 본 약관에 동의하게 되므로 본 약관을
@@ -18,13 +18,33 @@ class TermAndConditions extends Component {
           게시합니다.
         </p>
         <div>
-          <input id="terms" name="terms" type="checkbox" />
+          <input
+            id="terms"
+            name="terms"
+            type="checkbox"
+            onChange={this.handleCheck}
+          />
           <label htmlFor="terms">이용 약관에 동의합니다.</label>
         </div>
         <button type="submit">확인</button>
       </form>
     );
   }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  handleCheck = (e) => {
+    // 이벤트 핸들러 내부에는 [ 부수 효과 ]을 작성할 수 있다.
+    // 명령형 프로그래밍 (리액트 렌더링 프로세스와 관련 없음)
+    const submitButton = document.querySelector('[type="submit"]');
+    if (e.target.checked) {
+      submitButton.removeAttribute('disabled');
+    } else {
+      submitButton.setAttribute('disabled', 'true');
+    }
+  };
 }
 
 export default TermAndConditions;
