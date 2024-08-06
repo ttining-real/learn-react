d724cad5ed43660aa5cf8220b9403ad6bae78640
ttining <ttining.real@gmail.com>
2024-08-06 11:19:45 +0900

    [학습] 카운터 선언적 제어 IIl
    - 클래스 컴포넌트
    - constructor 함수의 역할 (초기화 설정: 상태, 메서드 this 바인딩)
    - 클래스 필드 구문 (화살표 함수, this 바인딩)
    - 파생된 상태 render() 메서드 내부에 추가 렌더링

--- 12-react-component-states/src/Playground.jsx
+++ 12-react-component-states/src/Playground.jsx
@@ -1,9 +1,11 @@
 import Counter from '@/components/Counter';
+import CounterClass from '@/components/Counter.class';
 
 function Playground() {
   return (
     <>
       <Counter count={3} min={2} max={6} />
+      <CounterClass min={9} count={10} step={101} />
       {/* <Counter count={10} step={4} min={20} max={40} /> */}
     </>
   );
