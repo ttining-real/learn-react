119ad2cc37254609350391c10ec59c38933bf75d
ttining <ttining.real@gmail.com>
2024-08-06 10:33:13 +0900

    [학습] 카운터 선언적 제어 II
    - 함수 컴포넌트
    - 훅 함수의 규칙
    - 상태 선언, 이벤트 핸들러, 파생된 상태 활용

--- 12-react-component-states/src/Playground.jsx
+++ 12-react-component-states/src/Playground.jsx
@@ -3,7 +3,8 @@ import Counter from '@/components/Counter';
 function Playground() {
   return (
     <>
-      <Counter count={10} step={4} min={20} max={40} />
+      <Counter count={3} min={2} max={6} />
+      {/* <Counter count={10} step={4} min={20} max={40} /> */}
     </>
   );
 }
--- 12-react-component-states/src/components/Counter.jsx
+++ 12-react-component-states/src/components/Counter.jsx
@@ -1,18 +1,19 @@
-// --------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // ✅ Counter 컴포넌트
-// --------------------------------------------------------------------------
-// - [ ] `count` 속성(prop, 기본 값: 1)을 전달받아 화면에 표시
-// - [ ] `step` 속성(기본 값: 1)을 전달받아 버튼 레이블에 표시
-// - [ ] `min` 속성(기본 값: 1) 보다 `count` 값이 크거나 같아야 함
-// - [ ] `max` 속성(기본 값: 1000) 보다 `count` 값이 작거나 같아야 함
-// - [ ] 사용자가 감소 버튼을 클릭하면 `count` 감소 (step 만큼)
-// - [ ] 사용자가 증가 버튼을 클릭하면 `count` 증가 (step 만큼)
-// - [ ] 사용자가 감소 버튼을 클릭했을 때 `count` 값이 `min` 보다 작을 경우 감소 버튼 비활성화
-// - [ ] 사용자가 증가 버튼을 클릭했을 때 `count` 값이 `max` 보다 클 경우 증가 버튼 비활성화
-// --------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+// - [x] `count` 속성(prop, 기본 값: 1)을 전달받아 화면에 표시
+// - [x] `step` 속성(기본 값: 1)을 전달받아 버튼 레이블에 표시
+// - [x] `min` 속성(기본 값: 1) 보다 `count` 값이 크거나 같아야 함
+// - [x] `max` 속성(기본 값: 1000) 보다 `count` 값이 작거나 같아야 함
+// - [x] 사용자가 감소 버튼을 클릭하면 `count` 감소 (step 만큼)
+// - [x] 사용자가 증가 버튼을 클릭하면 `count` 증가 (step 만큼)
+// - [x] 사용자가 감소 버튼을 클릭했을 때 `count` 값이 `min` 보다 작거나 같을 경우 감소 버튼 비활성화
+// - [x] 사용자가 증가 버튼을 클릭했을 때 `count` 값이 `max` 보다 크거나 같을 경우 증가 버튼 비활성화
+// ------------------------------------------------------------------------------
 
 import { useState } from 'react';
 import { number } from 'prop-types';
+import './Counter.css';
 
 Counter.propTypes = {
   count: number,
@@ -23,13 +24,54 @@ Counter.propTypes = {
 
 /**@type {({ count?: number, step?: number, min?: number, max?: number }) => JSX.Element} */
 function Counter({ count: initialCount = 1, step = 1, min = 1, max = 1000 }) {
-  const [count] = useState(initialCount);
+  // only state
+  // 참고: https://react.dev/reference/react/useState#usestate
+  const [count, setCount] = useState(() => {
+    if (initialCount < min || initialCount > max) {
+      throw new Error(`count 값이 min 보다 작거나, max보다 큽니다.`);
+    }
+
+    return initialCount;
+  });
+
+  // only updator function
+  // const [, setCount] = useState(initialCount);
+  // console.log(count, setCount);
+
+  const handleDecrease = () => {
+    // console.log('decrease count');
+    const nextCount = count - step;
+    setCount(nextCount);
+  };
+
+  const handleIncrease = () => {
+    // console.log('increase count');
+    const nextCount = count + step;
+    setCount(nextCount);
+  };
+
+  // 파생된 상태 (Derived States)
+  // (컴포넌트의 상태(states) 또는 속성(props)에 의해 (다시) 계산되는 값이 기억된 지역 변수)
+  const isDisabledDecrease = count <= min;
+  const isDisabledIncrease = count >= max;
 
   return (
     <div className="Counter">
-      <button type="button"> -{step} </button>
+      <button
+        type="button"
+        disabled={isDisabledDecrease}
+        onClick={handleDecrease}
+      >
+        -{step}
+      </button>
       <output>{count}</output>
-      <button type="button"> +{step} </button>
+      <button
+        type="button"
+        disabled={isDisabledIncrease}
+        onClick={handleIncrease}
+      >
+        +{step}
+      </button>
     </div>
   );
 }
