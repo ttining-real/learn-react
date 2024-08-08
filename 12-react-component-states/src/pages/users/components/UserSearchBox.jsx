c6311ee0ab98d26cdd660e1b21b3953cf90ecad0
ttining <ttining.real@gmail.com>
2024-08-08 09:23:13 +0900

    [학습] 추가 기능 구현 Part 1
    - 사용자 Enter 키 입력 시, 찾기
    - 찾기 실행 후, 검색 입력 필드 초기화
    - 사용자 입력 경고 후, 검색 필드에 초점 이동

--- 12-react-component-states/src/pages/users/components/UserSearchBox.jsx
+++ 12-react-component-states/src/pages/users/components/UserSearchBox.jsx
@@ -10,21 +10,34 @@ UserSearchBox.propTypes = {
 function UserSearchBox({ searchTerm, onSearch }) {
   const id = useId();
 
-  const handleSearch = () => {
+  const handleSearch = (e) => {
+    // 브라우저의 기본 작동 방지
+    // <form> 요소의 action에 설정된 주소로 폼 데이터 전송 시도
+    e.preventDefault();
+
     // Side Effects
     // DOM 접근, 속성 값 읽기
     const input = document.getElementById(id);
+    const button = input.closest('form').querySelector('[type="submit"]');
     const value = input.value.trim();
 
-    onSearch?.(value);
-    // if (value.length > 0) {
-    // } else {
-    //   alert('검색어를 입력해주세요.');
-    // }
+    if (value.length > 0) {
+      // 사용자 찾기 기능 실행
+      onSearch?.(value);
+      // 실행 이후, 검색 필드 초기화
+      input.value = '';
+      // 검색 기능은 찾기 버튼을 눌렀을 때
+      // 실행되므로 버튼 요소에 초점 이동
+      button.focus();
+    } else {
+      alert('검색어를 입력해주세요.');
+      input.value = '';
+      input.focus();
+    }
   };
 
   return (
-    <div className="UserSearchBox">
+    <form className="UserSearchBox" onSubmit={handleSearch}>
       <div className="control">
         <label htmlFor={id}>사용자 검색</label>
         <input
@@ -37,10 +50,8 @@ function UserSearchBox({ searchTerm, onSearch }) {
           // readOnly
         />
       </div>
-      <button type="button" onClick={handleSearch}>
-        찾기
-      </button>
-    </div>
+      <button type="submit">찾기</button>
+    </form>
   );
 }
 
