51b86d3c1737cf4aba8b079152a95f455185d804
ttining <ttining.real@gmail.com>
2024-08-11 21:18:21 +0900

    [과제] tic-tac-toe 게임 Part 5.
    - 게임 히스토리
    - 상태 끌어올리기

--- 00-homework/05-tic-tac-toe-game/src/tic-tac-toe/Game.jsx
+++ 00-homework/05-tic-tac-toe-game/src/tic-tac-toe/Game.jsx
@@ -2,12 +2,71 @@ import './styles/main.css';
 import S from './Game.module.css';
 import Board from '@/tic-tac-toe/components/Board/Board';
 import History from '@/tic-tac-toe/components/History/History';
+import { useState } from 'react';
+import {
+  PLAYER,
+  INITIAL_SQUARES,
+  PLAYER_COUNT,
+  checkedWinner,
+} from '@/tic-tac-toe/constants';
 
 function Game() {
+  // 게임 상태
+  const [gameHistory, setGameHistory] = useState([INITIAL_SQUARES]);
+
+  // 게임 순서
+  const [gameIndex, setGameIndex] = useState(0);
+
+  // 게임 상태 업데이트
+  const handlePlayGame = (index) => () => {
+    if (winnerInfo) {
+      alert('GAME OVER');
+      return;
+    }
+
+    const nextGameIndex = gameIndex + 1;
+    setGameIndex(nextGameIndex);
+
+    const nextSquares = currentSquares.map((square, idx) => {
+      return idx === index ? currentPlayer : square;
+    });
+
+    const nextGameHistory = [
+      ...gameHistory.slice(0, nextGameIndex),
+      nextSquares,
+    ];
+    setGameHistory(nextGameHistory);
+  };
+
+  const currentSquares = gameHistory[gameIndex];
+
+  const winnerInfo = checkedWinner(currentSquares);
+
+  // 다음 게임 순서
+  // const nextGameIndex = gameIndex + 1;
+  // setGameIndex(nextGameIndex);
+
+  // squares 배열에서 truthy 값의 수
+  // const gameIndex = squares.filter(Boolean).length;
+
+  // 현재 플레이어가 누구인지 계산
+  const isPlayerOneTurn =
+    currentSquares.filter(Boolean).length % PLAYER_COUNT === 0;
+  // 현재 플레이어
+  const currentPlayer = isPlayerOneTurn ? PLAYER.ONE : PLAYER.TWO;
+
+  const isDraw = !winnerInfo && currentSquares.every(Boolean);
+
   return (
     <div className={S.component}>
-      <Board />
-      <History />
+      <Board
+        squares={currentSquares}
+        winnerInfo={winnerInfo}
+        nextPlayer={currentPlayer}
+        onPlay={handlePlayGame}
+        isDraw={isDraw}
+      />
+      <History gameHistory={gameHistory} gameIndex={gameIndex} />
     </div>
   );
 }
--- 00-homework/05-tic-tac-toe-game/src/tic-tac-toe/components/History/History.jsx
+++ 00-homework/05-tic-tac-toe-game/src/tic-tac-toe/components/History/History.jsx
@@ -1,12 +1,29 @@
 import S from './History.module.css';
+import { arrayOf, func, number } from 'prop-types';
+import { OneOfPlayerListType } from '@/tic-tac-toe/types/type.d';
 
-function History() {
+History.propTypes = {
+  gameIndex: number.isRequired,
+  gameHistory: arrayOf(OneOfPlayerListType),
+  onTimeTravel: func,
+};
+
+function History({ gameIndex, gameHistory = [] }) {
   return (
     <div className={S.component}>
       <ol>
-        <li>
-          <button type="button">게임 시작</button>
-        </li>
+        {gameHistory.map((history, index) => {
+          const buttonLabel = index === 0 ? '게임 시작' : `게임 #${index}`;
+          const isDisabled = gameIndex === index;
+
+          return (
+            <li key={index}>
+              <button type="button" disabled={isDisabled}>
+                {buttonLabel}
+              </button>
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
