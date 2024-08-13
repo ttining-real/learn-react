62779aac97e7db8c6ad866c1c1e8259d230356ae
ttining <ttining.real@gmail.com>
2024-08-13 22:56:24 +0900

    [학습] 스크롤 애니메이션
    - 스크롤 진행률 애니메이션

--- 14-using-react-refs/src/routes/motion-one-in-scroll/components/ProgressBar.jsx
+++ 14-using-react-refs/src/routes/motion-one-in-scroll/components/ProgressBar.jsx
@@ -5,6 +5,8 @@
 // - [ ] 대상 요소의 스크롤 위치에 따라 아웃풋 텍스트 콘텐츠 값이 %로 출력되도록 설정합니다.
 // --------------------------------------------------------------------------
 
+import { useRef } from 'react';
+import { scroll } from 'motion';
 import { oneOf, string } from 'prop-types';
 import S from './Progress.module.css';
 
@@ -14,13 +16,35 @@ ProgressBar.propTypes = {
 };
 
 function ProgressBar({ containerSelector = null, axis = 'y' }) {
-  console.log(containerSelector, axis);
+  // console.log(containerSelector, axis);
+  const progressBarRef = useRef(null);
+  const outputRef = useRef(null);
+
+  // 사용자  액션 이벤트 X
+  // 마운트 시점에 실행될 콜백 함수 : ref callback
+  const setProgressBar = () => {
+    const container = document.querySelector(containerSelector);
+    const scrollOptions = { container, axis };
+
+    // 스크롤 애니메이션
+    scroll(({ y: { progress } }) => {
+      const progressBar = progressBarRef.current;
+      const output = outputRef.current;
+
+      if (progressBar && output) {
+        progressBarRef.current.style.transform = `scaleX(${progress})`;
+        output.value = (progress * 100).toFixed(0) + '%';
+      }
+    }, scrollOptions);
+  };
 
   return (
-    <>
-      <div className={S.progress} />
-      <output className={S.output}>0%</output>
-    </>
+    <div ref={setProgressBar}>
+      <div ref={progressBarRef} className={S.progress} />
+      <output ref={outputRef} className={S.output}>
+        0%
+      </output>
+    </div>
   );
 }
 
