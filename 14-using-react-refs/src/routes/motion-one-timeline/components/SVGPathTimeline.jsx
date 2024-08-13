a452e70c03cb6a885611d6ec2b9d5761d5ee0757
ttining <ttining.real@gmail.com>
2024-08-13 22:34:22 +0900

    [학습] 타임라인 애니메이션 l
    - 멀티 요소에 animate() / timeline() 함수 적용

--- 14-using-react-refs/src/routes/motion-one-timeline/components/SVGPathTimeline.jsx
+++ 14-using-react-refs/src/routes/motion-one-timeline/components/SVGPathTimeline.jsx
@@ -1,13 +1,15 @@
 // --------------------------------------------------------------------------
 // ✅ SVG 패스 타임라인 애니메이션
 // --------------------------------------------------------------------------
-// - [ ] 스타일 모듈에서 stroke-dasharray, visibility 값 설정을 확인합니다.
+// - [ ] 스타일 모듈에서 stroke-dasharray, stroke-dashoffset, visibility 값 설정을 확인합니다.
 // - [ ] 사용자가 버튼을 클릭하면, SVG 패스 애니메이션이 표시되도록 설정합니다.
 //   - [ ] 타임라인 시퀀스(배열)를 설정합니다.
 //   - [ ] circle → path 순으로 움직이도록 설정합니다.
 //   - [ ] strokeDashoffset 속성에 키프레임을 사용하세요. (1 → 0)
 // --------------------------------------------------------------------------
 
+import { useRef } from 'react';
+import { animate, timeline } from 'motion';
 import { number } from 'prop-types';
 import S from './SVGPathTimeline.module.css';
 
@@ -16,7 +18,42 @@ SVGPathTimeline.propTypes = {
 };
 
 function SVGPathTimeline({ size = 60 }) {
-  const handleTimelineAnimate = () => {};
+  const svgCircleRef = useRef(null);
+  const svgPathRef = useRef(null);
+
+  const handleTimelineAnimate = () => {
+    const circleElement = svgCircleRef.current;
+    const pathElement = svgPathRef.current;
+
+    // #1 : animate() 함수 사용
+    // animate(
+    //   circleElement,
+    //   { strokeDashoffset: [1, 0], visibility: 'visible' },
+    //   { duration: 0.8, easing: 'ease-in-out' }
+    // );
+
+    // animate(
+    //   pathElement,
+    //   { strokeDashoffset: [1, 0], visibility: 'visible' },
+    //   { duration: 0.6, easing: 'ease-in-out', delay: 0.6 }
+    // );
+
+    // #2 : timeline() 함수 사용
+    const sequence = [
+      [
+        circleElement,
+        { strokeDashoffset: [1, 0], visibility: 'visible' },
+        { duration: 1, easing: 'ease-out' },
+      ],
+      [
+        pathElement,
+        { strokeDashoffset: [1, 0], visibility: 'visible' },
+        { duration: 0.6, easing: 'ease-in-out', at: '-0.2' },
+      ],
+    ];
+
+    timeline(sequence);
+  };
 
   return (
     <div className={S.component}>
@@ -29,8 +66,16 @@ function SVGPathTimeline({ size = 60 }) {
       </button>
 
       <svg width={size} height={size} viewBox="0 0 200 200">
-        <circle className={S.circle} cx="100" cy="100" r="80" pathLength="1" />
+        <circle
+          ref={svgCircleRef}
+          className={S.circle}
+          cx="100"
+          cy="100"
+          r="80"
+          pathLength="1"
+        />
         <path
+          ref={svgPathRef}
           className={S.path}
           d="M 54 107.5 L 88 138.5 L 144.5 67.5"
           pathLength="1"
