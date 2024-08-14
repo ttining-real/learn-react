e57206f7aa451d7f3f3d64fc0ac695af2a63b168
ttining <ttining.real@gmail.com>
2024-08-14 16:13:42 +0900

    [학습] 채팅 윈도우 구현 II
    - 키다운 이벤트 핸들링
    - Enter 키 누르면 화면 업데이트 요청

--- 14-using-react-refs/src/routes/using-imperative-handle/components/ChatWindow.jsx
+++ 14-using-react-refs/src/routes/using-imperative-handle/components/ChatWindow.jsx
@@ -18,28 +18,62 @@ ChatWindow.propTypes = {
 function ChatWindow({ messages, onAddMessage }) {
   const id = useId();
   const textareaRef = useRef(null);
+  const olRef = useRef(null);
 
   const handleSendMessage = (e) => {
     e.preventDefault();
-
     const formData = new FormData(e.currentTarget);
-
     let newMessage = formData.get('message');
     newMessage = newMessage.trim();
 
+    sendMessage(newMessage);
+  };
+
+  const sendMessage = (newMessage) => {
+    const textarea = textareaRef.current;
+    const ol = olRef.current;
+
     if (newMessage.length <= 0) {
       alert('메시지 내용을 입력하세요!');
-      textareaRef.current.select();
+      textarea.select();
     }
 
+    // 리액트는 상태를 동기적으로 업데이트 하지 않음
+    // 리액트는 상태 업데이트 요청을 지켜보고, 효과적으로
+    // 배치(batch, 일괄) 업데이트 합니다.
     onAddMessage?.(newMessage);
+
+    textarea.value = '';
+
+    // 타이머를 사용하지 않은 경우
+    // ol.scrollTo(0, ol.scrollHeight);
+
+    // 타이머 (우회적으로 리액트의 권장 방법이 아닌 방법으로 문제 해결)
+    setTimeout(() => ol.scrollTo(0, ol.scrollHeight));
+  };
+
+  // 이펙트 학습 후에 리-렌더 이후 화면에 반영되면 그 때 끌어내려~
+  // ol.scrollTo(0, ol.scrollHeight);
+
+  const handleKeyDown = (e) => {
+    const { key, shiftKey } = e;
+
+    if (key === 'Enter' && !shiftKey) {
+      e.preventDefault();
+
+      const newMessage = e.currentTarget.value.trim();
+
+      if (newMessage.length > 0) {
+        sendMessage(newMessage);
+      }
+    }
   };
 
   return (
     <section className={S.component}>
       <h2 className="sr-only">채팅 화면</h2>
 
-      <ol className={S.chats}>
+      <ol ref={olRef} className={S.chats}>
         {messages.map(({ id, isMe, message }) => {
           const classNames = `${S.chat} ${isMe ? S.me : ''}`.trim();
 
@@ -56,7 +90,12 @@ function ChatWindow({ messages, onAddMessage }) {
           <label htmlFor={id} className="sr-only">
             메시지 입력
           </label>
-          <textarea ref={textareaRef} id={id} name="message" />
+          <textarea
+            ref={textareaRef}
+            id={id}
+            name="message"
+            onKeyDown={handleKeyDown}
+          />
         </div>
         <button type="submit">보내기</button>
       </form>
