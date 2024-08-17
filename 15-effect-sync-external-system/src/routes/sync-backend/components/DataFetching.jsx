fcede5ad5795eb7aa5a8da9317213cf9f3c94800
ttining <ttining.real@gmail.com>
2024-08-18 04:46:53 +0900

    [학습] 데이터 패칭(가져오기) V
    - ESLint 규칙 억제하지 말 것!
    - 무조건 ESLint 규칙을 믿지도 말 것!
    - 무한 반복 주의!!!!!
    - setState((previousState) => nextState) API 활용 (현재 컴포넌트의 스냅샷 데이터인 상태 의존도 제거)

--- 15-effect-sync-external-system/src/routes/sync-backend/components/DataFetching.jsx
+++ 15-effect-sync-external-system/src/routes/sync-backend/components/DataFetching.jsx
@@ -7,18 +7,33 @@ import { exact, string } from 'prop-types';
 const ENDPOINT = '//yamoo9.pockethost.io/api/collections/olive_oil/records';
 
 function DataFetching() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [data, setData] = useState(null);
 
-  // 마운트 -> 언마운트 -> 리-마운트
-  // 중복된 네트워크 요청 중단
-  // 네트워크 요청 1회 성공 시
-  // 상태 업데이트 1회 진행
+  const [state, setState] = useState({
+    isLoading: false,
+    error: null,
+    data: null,
+  });
+
   useEffect(() => {
     const abortController = new AbortController();
 
-    setIsLoading(true);
+    // API 1
+    // setState(nextState);
+    // setState({ ...state, key: value });
+
+    // setIsLoading(true);
+
+    // API 2
+    // setState((previousState) => nextState);
+    // setState((prevState) => ({ ...prevState, key: value }));
+
+    setState((prevState) => ({
+      ...prevState,
+      isLoading: true,
+    }));
 
     const fetchOliveOil = async () => {
       try {
@@ -32,43 +47,57 @@ function DataFetching() {
           throw new Error(responseData.message);
         }
 
-        setData(responseData);
-        setIsLoading(false);
+        // setData(responseData);
+        // setIsLoading(false);
+        // setState({
+        //   ...state,
+        //   data: responseData,
+        //   isLoading: false,
+        // });
+
+        setState((prevState) => ({
+          ...prevState,
+          data: responseData,
+          isLoading: false,
+        }));
       } catch (error) {
-        // 중복된 요청 취소를 오류로 보지 않음
-        // 그 이외의 오류가 발생한 경우 오류로 봄
         if (!(error instanceof DOMException)) {
-          setError(error);
-          setIsLoading(false);
+          // setError(error);
+          // setIsLoading(false);
+          // setState({
+          //   ...state,
+          //   error,
+          //   isLoading: false,
+          // });
+
+          setState((prevState) => ({
+            ...prevState,
+            error,
+            isLoading: false,
+          }));
         }
       }
     };
 
     fetchOliveOil();
 
-    // 클린업 함수
-    // 마운트(이펙트 콜백 실행) -> 언마운트(클린업 실행) -> 리-마운트(이펙트 콜백 실행)
     return () => {
-      // AbortController 인스턴스(객체)의
-      // abort() 메서드를 사용해 이전 요청을 중단
       abortController.abort();
     };
   }, []);
 
-  if (isLoading) {
-    console.log('isLoading');
-
+  if (state.isLoading) {
     return <LoadingMessage />;
   }
 
-  if (error) {
-    return <PrintError error={error} />;
+  if (state.error) {
+    return <PrintError error={state.error} />;
   }
 
   return (
     <div className={S.component}>
       <ul>
-        {data?.items.map?.((item) => (
+        {state.data?.items.map?.((item) => (
           <li key={item.id}>{item.name}</li>
         ))}
       </ul>
