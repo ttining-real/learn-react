99d51500d416debe3608705142cc624b5b1231ca
ttining <ttining.real@gmail.com>
2024-08-18 04:40:26 +0900

    [학습] 이펙트 동기화 / 정리 IV
    - 클린업 함수의 중요성

--- 15-effect-sync-external-system/src/routes/effect-sync-and-cleanup/components/ClockOnOff.jsx
+++ 15-effect-sync-external-system/src/routes/effect-sync-and-cleanup/components/ClockOnOff.jsx
@@ -1,11 +1,11 @@
 // --------------------------------------------------------------------------
 // ✅ 클록(시계) ON/OFF // 정리
 // --------------------------------------------------------------------------
-// - [ ] 1초마다 시간 정보를 화면에 업데이트하도록 이펙트를 작성합니다. (타이머 API 사용)
+// - [x] 1초마다 시간 정보를 화면에 업데이트하도록 이펙트를 작성합니다. (타이머 API 사용)
 // - [ ] 컴포넌트가 언마운트 된 이,후 남은 이펙트를 깨끗하게 정리합니다.
 // --------------------------------------------------------------------------
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { bool, func } from 'prop-types';
 import S from './ClockOnOff.module.css';
 
@@ -15,7 +15,24 @@ ClockOnOff.propTypes = {
 };
 
 function ClockOnOff({ isOn = false, onToggle }) {
-  const [time] = useState(new Date());
+  const [time, setTime] = useState(new Date());
+
+  // 이펙트 추가
+  useEffect(() => {
+    // 1초 마다 로케일 타임 업데이트
+    console.log('타이머 연결');
+    const clearId = setInterval(() => {
+      const nextTime = new Date();
+      console.log(nextTime.toLocaleTimeString());
+      setTime(nextTime);
+    }, 1000);
+
+    // 클린업 필요!
+    return () => {
+      console.log('타이머 해지');
+      clearInterval(clearId);
+    };
+  }, []);
 
   return (
     <div className={S.component}>
