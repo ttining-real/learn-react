512b6ab3f4ac67c9a4b8dfa52641d546399b7903
ttining <ttining.real@gmail.com>
2024-08-18 04:41:04 +0900

    [학습] 이펙트 동기화 / 정리 V
    - 상태 변경 → 이펙트 콜백 실행 (종속성이 상태로 설정된 경우)
    - 만약 무한 루프에 빠질 경우 패닉!! (조건 처리 필요)
    - 클린업(정리) 매우 중요!

--- 15-effect-sync-external-system/src/routes/effect-sync-and-cleanup/components/UselessCheckbox.jsx
+++ 15-effect-sync-external-system/src/routes/effect-sync-and-cleanup/components/UselessCheckbox.jsx
@@ -1,16 +1,39 @@
 // --------------------------------------------------------------------------
 // ✅ 쓸모없는 체크박스 / 정리
 // --------------------------------------------------------------------------
-// - [ ] 체크 상태 해제해도, 1초 뒤 다시 체크되는 쓸모없는 체크박스를 구현합니다. (타이머 API 사용)
-// - [ ] 컴포넌트가 언마운트 된 이,후 남은 이펙트를 깨끗하게 정리합니다.
+// - [x] 체크 상태 해제해도, 1초 뒤 다시 체크되는 쓸모없는 체크박스를 구현합니다. (타이머 API 사용)
+// - [x] 컴포넌트가 언마운트 된 이후, 남은 이펙트를 깨끗하게 정리합니다.
 // --------------------------------------------------------------------------
 
-import { useId, useState } from 'react';
+import { useEffect, useId, useState } from 'react';
 import S from './UselessCheckbox.module.css';
 
 function UselessCheckbox() {
   const id = useId();
-  const [checked, setChecked] = useState(true);
+
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    // 이펙트 콜백 함수 안에서
+    // 상태 업데이트 조건 처리
+
+    let clearId = 0;
+
+    // checked = false 일 때만 실행
+    if (checked === false) {
+      console.log('타이머 연결');
+      clearId = setTimeout(() => {
+        setChecked(true);
+        console.log('쓸데없는 기능 실행');
+      }, 1000);
+    }
+
+    // checked = true 일 때는 무시
+
+    return () => {
+      console.log('타이머 클린업');
+      clearTimeout(clearId);
+    };
+  }, [checked]);
 
   const handleChecked = () => setChecked((c) => !c);
 
