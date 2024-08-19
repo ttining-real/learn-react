b0c852c78ac9a2d613a67c4d6aad7cc25885f4c8
ttining <ttining.real@gmail.com>
2024-08-19 19:40:59 +0900

    [학습] 커스텀 훅 2
    - useEventListner() 훅
    - 이펙트 콜백 관리 훅 (자동 연결, 해지 기능)
    - 다만, 함수 참조가 동일해야 함 (useCallback 훅 활용)

---
+++ 16-using-custom-hooks/src/hooks/useEventListener.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+
+/** @type { (target: Document | HTMLElement, eventType: string, eventHandler: (e: Event) => void) => void} */
+function useEventListener(target, eventType, eventHandler) {
+  useEffect(() => {
+    console.log('이벤트 구독');
+    target.addEventListener(eventType, eventHandler);
+
+    return () => {
+      console.log('이벤트 구독 해지');
+      target.removeEventListener(eventType, eventHandler);
+    };
+  }, [target, eventType, eventHandler]);
+}
+
+export default useEventListener;
--- 16-using-custom-hooks/src/routes/effect-sync-and-cleanup/components/PrintMousePosition.jsx
+++ 16-using-custom-hooks/src/routes/effect-sync-and-cleanup/components/PrintMousePosition.jsx
@@ -1,25 +1,32 @@
-import { useEffect, useState } from 'react';
-import S from './PrintMousePosition.module.css';
 import useDocumentTitle from '@/hooks/useDocumentTitle';
+import useEventListener from '@/hooks/useEventListener';
+import { useCallback, useState } from 'react';
+import S from './PrintMousePosition.module.css';
 
 function PrintMousePosition() {
   const documentTitle = '마우스 위치 추적 ← 이펙트 동기화 & 정리';
   useDocumentTitle(documentTitle);
 
+  /* -------------------------------------------------------------------------- */
+
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const { x, y } = mousePosition;
 
-  useEffect(() => {
-    const handleMove = ({ pageX: x, pageY: y }) => {
-      setMousePosition({ x, y });
-    };
+  // const eventHandler = ({ pageX: x, pageY: y }) => {
+  //   // 상태 업데이트 -> 렌더 트리거 -> 컴포넌트 리-렌더링(함수 재실행)
+  //   setMousePosition({ x, y });
+  // };
 
-    document.addEventListener('mousemove', handleMove);
+  // useEventListener(document, 'mousemove', eventHandler);
 
-    return () => {
-      document.removeEventListener('mousemove', handleMove);
-    };
+  // 처음 정의된 이후, 종속성 배열이 비어 있기 때문에
+  // 이 함수 참조는 리-렌더링 과정에서 항상 동일하다. (변경되지 않는다.)
+  const onMouseTracking = useCallback(({ pageX: x, pageY: y }) => {
+    // 상태 업데이트 -> 렌더 트리거 -> 컴포넌트 리-렌더링(함수 재실행)
+    setMousePosition({ x, y });
   }, []);
+
+  useEventListener(document, 'mousemove', onMouseTracking);
 
   return (
     <div className={S.component}>
