2c07db5efaa53c8efcaff23b77fbe60482931d88
ttining <ttining.real@gmail.com>
2024-08-19 11:03:33 +0900

    [학습] 노트 작성(읽기)
    - 서버의 노트 데이터 모두 가져오기

--- 15-effect-sync-external-system/src/api/notes.js
+++ 15-effect-sync-external-system/src/api/notes.js
@@ -2,7 +2,7 @@
 // ✅ 클라이언트 측 Notes API (CRUD)
 // --------------------------------------------------------------------------
 // - [x] 생성(Create)
-// - [ ] 읽기(Read)
+// - [x] 읽기(Read)
 // - [ ] 수정(Update)
 // - [ ] 삭제(Delete)
 // --------------------------------------------------------------------------
@@ -11,18 +11,19 @@
 const ENDPOINT = 'http://127.0.0.1:8090';
 const REQUEST_OPTIONS = {
   headers: {
-    'Content-type': 'application/json',
+    'Content-Type': 'application/json',
   },
 };
 
-// 비동기 함수 작성
+// 비동기(async) 함수 작성
 // Create
-/** @type {(newNote: { title: string, description: string })} */
+/** @type {(newNote: { title: string, description: string }) => Promise<any>} */
 export async function createNote(newNote) {
   // 외부 시스템(서버)에 데이터 생성 요청
   // POST 요청 URL
   const REQUEST_URL = `${ENDPOINT}/api/collections/notes/records`;
-  // POST 요청 시, 전송할 JSON 포맷 문자열
+
+  // POST 요청 시, 전송할 JSON 포멧 문자열
   const body = JSON.stringify({
     title: newNote.title,
     description: newNote.description,
@@ -35,6 +36,7 @@ export async function createNote(newNote) {
     ...REQUEST_OPTIONS,
   });
 
+  // 에러 핸들링
   if (!response.ok) {
     throw new Response(
       JSON.stringify({ message: '서버에서 요청에 응답하지 않습니다.' }),
@@ -48,7 +50,23 @@ export async function createNote(newNote) {
 }
 
 // Read
-export async function readNotes() {}
+/** @type {(page?: number, perPage?: number) => Promise<any>} */
+export async function readNotes(page = 1, perPage = 10) {
+  const REQUEST_URL = `${ENDPOINT}/api/collections/notes/records?page=${page}&perPage=${perPage}`;
+  const response = await fetch(REQUEST_URL);
+
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({ message: '서버에서 요청에 응답하지 않습니다.' }),
+      { status: 500 }
+    );
+  }
+
+  const responseData = await response.json();
+
+  return responseData;
+}
+
 export async function readNoteOne() {}
 
 // Update
--- 15-effect-sync-external-system/src/routes/sync-backend/components/DataMutation.jsx
+++ 15-effect-sync-external-system/src/routes/sync-backend/components/DataMutation.jsx
@@ -1,34 +1,75 @@
 // --------------------------------------------------------------------------
 // ✅ 데이터 뮤테이션
 // --------------------------------------------------------------------------
-// - [ ] PocketBase 백엔드 솔루션을 서버로 사용합니다.
-// - [ ] 이벤트를 사용해 Promise 또는 Async / await 방법으로 데이터 뮤테이션을 요청합니다.
+// - [x] PocketBase 백엔드 솔루션을 서버로 사용합니다.
+// - [x] 이벤트를 사용해 Promise 또는 Async / await 방법으로 데이터 뮤테이션을 요청합니다.
 // - [ ] 데이터 뮤테이션 요청 응답이 성공인 경우, 리액트 앱 화면을 업데이트 합니다.
 // - [ ] 데이터 뮤테이션 요청 응답에 문제가 발생한 경우, 오류 메시지를 렌더링합니다.
 // --------------------------------------------------------------------------
 
-import { createNote } from '@/api/notes';
+import { createNote, readNotes } from '@/api/notes';
 import S from './DataMutation.module.css';
+import { useRef } from 'react';
 
 function DataMutation() {
+  const formRef = useRef(null);
+
+  const handleReadNotes = async () => {
+    const responseData = await readNotes();
+    console.log(responseData);
+  };
+
   const handleCreate = async () => {
-    // (폼: 사용자 입력) 더미 새 노트
-    const newNote = {
-      title: '리액트 마지막 주차 학습',
-      description:
-        '리액트의 마지막 학습 주제는 리액트의 에코 시스템에 대해 다뤄봅니다.',
-    };
+    // 폼 요소에 접근 => 폼 데이터 객체 생성
+    const formElement = formRef.current;
+    const formData = new FormData(formElement);
+
+    // 폼 데이터의 입력 값 가져오기
+    const title = formData.get('title');
+    const description = formData.get('description');
+
+    // 서버에 요청 전송할 새 노트 객체 생성
+    const newNote = { title, description };
 
     // 서버(외부 시스템) 요청/응답
-    await createNote(newNote);
+    const responseData = await createNote(newNote);
+    console.log(responseData);
+
+    // 응답이 성공하면 폼 초기화
+    formElement.reset();
   };
 
   return (
     <div className={S.component}>
-      {/* <p>서버에 데이터 생성/수정/삭제 요청한 후, 앱 화면 업데이트</p> */}
-      <button type="button" onClick={handleCreate}>
-        노트 작성
-      </button>
+      <form ref={formRef}>
+        <div>
+          <label htmlFor="noteTitle">제목</label>
+          <input type="text" id="noteTitle" name="title" />
+        </div>
+        <div>
+          <label htmlFor="noteDescription">내용</label>
+          <textarea
+            id="noteDescription"
+            name="description"
+            cols={20}
+            rows={3}
+          />
+        </div>
+      </form>
+      <div
+        role="group"
+        style={{
+          display: 'flex',
+          gap: 8,
+        }}
+      >
+        <button type="button" onClick={handleCreate}>
+          노트 작성
+        </button>
+        <button type="button" onClick={handleReadNotes}>
+          노트 읽기
+        </button>
+      </div>
     </div>
   );
 }
