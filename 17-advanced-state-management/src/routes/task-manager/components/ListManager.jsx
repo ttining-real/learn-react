5de3006359500fe899f9d7d512f2f6d32df3fedf
ttining <ttining.real@gmail.com>
2024-08-22 17:16:06 +0900

    [학습] 리듀서를 사용한 상태 관리 6
    - 리스트 상태 관리
    - useReducer() 훅 활용

--- 17-advanced-state-management/src/routes/task-manager/components/ListManager.jsx
+++ 17-advanced-state-management/src/routes/task-manager/components/ListManager.jsx
@@ -1,25 +1,63 @@
-/* eslint-disable no-unused-vars */
 import { useReducer } from 'react';
-import listReducer from '@/stores/list';
+import listReducer, {
+  addItem,
+  deleteItem,
+  editItem,
+  filterList,
+  resetList,
+  INITIAL_LIST,
+} from '@/stores/list';
 
 function ListManager() {
-  const [list] = useReducer(listReducer, ['one', 'zero']);
+  const [list, dispatch] = useReducer(listReducer, INITIAL_LIST);
 
-  const handleAddItem = () => {};
+  const handleAddItem = () => {
+    dispatch(addItem(crypto.randomUUID().split('-').at(0)));
+  };
 
-  const handleEditItem = () => {};
+  const handleEditItem = (editId) => {
+    dispatch(editItem(editId, crypto.randomUUID().split('-').at(0)));
+  };
 
-  const handleRemoveItem = () => {};
+  const handleDeleteItem = (deleteId) => {
+    dispatch(deleteItem(deleteId));
+  };
 
-  const handleFilterItems = () => {};
+  const handleFilterList = () => {
+    dispatch(filterList(Math.floor(Math.random() * 10)));
+  };
+
+  const handleResetList = () => {
+    dispatch(resetList());
+  };
 
   return (
     <div>
-      <h2 className="headline2">배열 관리자</h2>
+      <h2 className="headline2">리스트 관리자</h2>
+
+      <div role="group">
+        <button type="button" onClick={handleAddItem}>
+          추가
+        </button>
+        <button type="button" onClick={handleFilterList}>
+          필터링
+        </button>
+        <button type="button" onClick={handleResetList}>
+          리셋
+        </button>
+      </div>
 
       <ul>
         {list.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>
+            {item}
+            <button type="button" onClick={() => handleEditItem(index)}>
+              수정
+            </button>
+            <button type="button" onClick={() => handleDeleteItem(index)}>
+              삭제
+            </button>
+          </li>
         ))}
       </ul>
     </div>
--- 17-advanced-state-management/src/stores/list.js
+++ 17-advanced-state-management/src/stores/list.js
@@ -1,8 +1,86 @@
-// 추가
-// 수정
-// 삭제
-// 필터링
+/* 액션 ----------------------------------------------------------------------- */
 
-const listReducer = (state) => state;
+const ACTION_TYPES = {
+  ADD: '추가',
+  EDIT: '수정',
+  DELETE: '삭제',
+  FILTER: '필터링',
+  RESET: '초기화',
+};
+
+/* 액션 크리에이터 --------------------------------------------------------------- */
+
+export const addItem = (newItem) => ({
+  type: ACTION_TYPES.ADD,
+  payload: newItem,
+});
+
+export const editItem = (editId, nextItem) => ({
+  type: ACTION_TYPES.EDIT,
+  payload: { editId, nextItem },
+});
+
+export const deleteItem = (deleteId) => ({
+  type: ACTION_TYPES.DELETE,
+  payload: deleteId,
+});
+
+export const filterList = (filterKey) => ({
+  type: ACTION_TYPES.FILTER,
+  payload: filterKey,
+});
+
+export const resetList = () => ({
+  type: ACTION_TYPES.RESET,
+});
+
+/* 리듀서 ---------------------------------------------------------------------- */
+// 액션이란? "작업 요청서" action = {type, payload?}
+
+const generateItem = () => crypto.randomUUID().split('-').at(0);
+
+export const INITIAL_LIST = Array(3)
+  .fill(null)
+  .map(() => generateItem());
+
+const listReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.ADD: {
+      const nextState = [action.payload, ...state];
+      return nextState;
+    }
+
+    case ACTION_TYPES.EDIT: {
+      const { editId, nextItem } = action.payload;
+
+      const nextState = state.map((item, index) => {
+        return index !== editId ? item : nextItem;
+      });
+
+      return nextState;
+    }
+
+    case ACTION_TYPES.DELETE: {
+      const deleteId = action.payload;
+      const nextState = state.filter((item, index) => index !== deleteId);
+
+      return nextState;
+    }
+
+    case ACTION_TYPES.FILTER: {
+      const filterKey = action.payload.toString();
+      const nextState = state.filter((item) => item.includes(filterKey));
+      return nextState;
+    }
+
+    case ACTION_TYPES.RESET: {
+      return INITIAL_LIST;
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
 
 export default listReducer;
