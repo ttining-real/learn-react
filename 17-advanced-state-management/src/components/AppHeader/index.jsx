03fbc2d3e4a8223da0fb55bb81324796218a114b
ttining <ttining.real@gmail.com>
2024-08-23 18:16:37 +0900

    [학습] 인증 컨텍스트 & 리듀서 (상태 관리) 6
    - 로그아웃 기능 구현
    - 인증 컨텍스트 로그아웃 (초기화)
    - 포켓베이스 로그아웃 기능 실행

--- 17-advanced-state-management/src/components/AppHeader/index.jsx
+++ 17-advanced-state-management/src/components/AppHeader/index.jsx
@@ -1,22 +1,43 @@
 import { Link } from 'react-router-dom';
 import { IoLogoReact } from 'react-icons/io5';
+import { useAuth } from '@/contexts/auth';
 import S from './style.module.css';
+import pb from '@/api/pb';
+
+const DUMMY_IMAGE_URL = 'https://placehold.co/20x20?text=photo';
 
 function AppHeader() {
-  const authUser = true;
+  const { authInfo, resetAuth } = useAuth();
+
+  const user = authInfo.user;
+
+  const handleSignOut = () => {
+    // 컨텍스트 사용자 정보 초기화
+    resetAuth();
+    // 포켓베이스 로그아웃 기능 실행
+    pb.authStore.clear();
+  };
+
+  let profileImageUrl = DUMMY_IMAGE_URL;
+
+  if (user) {
+    profileImageUrl = pb.files.getUrl(user, user.avatar);
+  }
 
   return (
     <header className={S.component}>
       <Link to="/" className={S.homeLink} aria-label="홈">
         <IoLogoReact />
       </Link>
-      {authUser && (
+      {user && (
         <div className={S.wrapper}>
           <div className={S.userInfo}>
-            <img src="https://placehold.co/20x20?text=photo" alt="" />
-            사용자 이름
+            <img src={profileImageUrl} alt="" />
+            {user.username}
           </div>
-          <button type="button">로그아웃</button>
+          <button type="button" onClick={handleSignOut}>
+            로그아웃
+          </button>
         </div>
       )}
     </header>
